2fa6d554d42f3335eb5918c7df7f47201ae1280b
Senith <senithdilitha@gmail.com>
2022-10-25 23:24:40 +0530

    my first commit

---
+++ Models/PaymentDetailContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Payment_API.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+    }
+}
